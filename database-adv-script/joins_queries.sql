42d2f8d837d6a33966179cecd55a24beaa227def
Michael Atolwa <atolwamichael7@gmail.com>
2025-05-25 12:17:25 +0300

    update

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -5,6 +5,7 @@ INNER JOIN users u ON b.user_id = u.id;
 SELECT p.*, r.*
 FROM properties p
 LEFT JOIN reviews r ON p.id = r.property_id;
+ORDER BY p.property_id
 
 -- For PostgreSQL (direct syntax):
 SELECT u.*, b.*
